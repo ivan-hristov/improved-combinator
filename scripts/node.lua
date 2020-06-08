43d74750104ffd2b60a956255edfda246758d7c0
Ivan Hristov <Ivan.Hristov@Clearswift.com>
2020-06-08 22:59:47 +0100

    moved the event functions into the node class

--- scripts/node.lua
+++ scripts/node.lua
@@ -1,6 +1,5 @@
 local constants = require("constants")
 local uid = require("scripts.uid")
-local events = require("scripts.events")
 local logger = require("scripts.logger")
 
 local node = {}
@@ -179,17 +178,71 @@ function node:setup_events(node_param)
         return
     elseif node_param.events_id.on_click then
         if node_param.events_id.on_click == "on_click_new_task_button" then
-            node_param.events.on_click = events.on_click_new_task_button
+            node_param.events.on_click = node.on_click_new_task_button
         elseif node_param.events_id.on_click == "on_click_close_button" then
-            node_param.events.on_click = events.on_click_close_button
+            node_param.events.on_click = node.on_click_close_button
         end
     elseif node_param.events_id.on_selection_state_changed then
         if node_param.events_id.on_selection_state_changed == "on_selection_changed_task_dropdown" then
             node_param.events.on_selection_state_changed = {}
-            node_param.events.on_selection_state_changed[1] = events.on_selection_repeatable_timer
-            node_param.events.on_selection_state_changed[2] = events.on_selection_single_timer
+            node_param.events.on_selection_state_changed[1] = node.on_selection_repeatable_timer
+            node_param.events.on_selection_state_changed[2] = node.on_selection_single_timer
         end
     end
+end
+
+function node.on_click_new_task_button(event, node_param)
+    if event.element.parent[node_param.events_params.task_dropdown_frame_id].visible then
+        event.element.parent[node_param.events_params.task_dropdown_frame_id].visible = false
+    else
+        event.element.parent[node_param.events_params.task_dropdown_frame_id].visible = true
+    end
+end
+
+function node.on_click_close_button(event, node_param)
+    node_param.parent:remove()
+    event.element.parent.destroy()
+end
+
+function node.on_selection_repeatable_timer(event, node_param)
+    event.element.parent.visible = false
+    event.element.selected_index = 0
+
+    -- Setup Persistent Nodes --
+    local scroll_pane_node = node_param.parent.parent
+    local scroll_pane_gui = event.element.parent.parent
+
+    local repeatable_time_node = scroll_pane_node:add_child()
+    repeatable_time_node.gui = {
+        type = "frame",
+        direction = "vertical",
+        name = repeatable_time_node.id,
+        style = constants.style.conditional_frame
+    }
+
+    local close_button_node = repeatable_time_node:add_child()
+    close_button_node.gui = {
+        type = "sprite-button",
+        direction = "vertical",
+        name = close_button_node.id,
+        style = constants.style.close_button_frame,
+        sprite = "utility/close_white",
+        hovered_sprite = "utility/close_black",
+        clicked_sprite = "utility/close_black"
+    }
+    close_button_node.events_id.on_click = "on_click_close_button"
+
+    -- Setup Node Events --
+    close_button_node:recursive_setup_events()
+
+    -- Setup Factorio GUI --
+    local repeatable_time_gui = scroll_pane_gui.add(repeatable_time_node.gui)
+    repeatable_time_gui.add(close_button_node.gui)
+end
+
+function node.on_selection_single_timer(event, node_param)
+    event.element.parent.visible = false
+    event.element.selected_index = 0
 end
 
 
