2301e8cfc498a86265d340a5982641eefbe01772
Ivan Hristov <Ivan.Hristov@Clearswift.com>
2020-09-04 18:30:54 +0100

    Added a missing variable

--- migrations/ImprovedCombinator_1.0.11.lua
+++ migrations/ImprovedCombinator_1.0.11.lua
@@ -4,6 +4,7 @@ for entity_id, entity in pairs(global.entities) do
     for entity_id, entity in pairs(global.entities) do
         for _, elem in pairs(entity.update_list) do
             for _, child_elem in pairs(elem.data.children) do
+                local update_logic = child_elem.data.update_logic
                 if update_logic.decider_combinator and update_logic.decider_method then
                     if update_logic.decider_method >= 5 then
                         update_logic.decider_method = update_logic.decider_method  + 1
